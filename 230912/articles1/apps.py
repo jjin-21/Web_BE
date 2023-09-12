3ec48a3c2e30d4048bc1480c2de4fd08cccb01d2
jjin-21 <ljj1900@naver.com>
2023-09-12 17:58:54 +0900

    230912

---
+++ 230912/articles1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Articles1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'articles1'
---
+++ 230912/articles4/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Articles4Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'articles4'
