41a26fa2d18609d58863533cc910badc93eff314
jjin-21 <ljj1900@naver.com>
2023-09-13 17:58:04 +0900

    230913

---
+++ 1.Django Intro&HTML Form/1-2/calculators/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def calculation(request):
+    return render(request, 'calculators/calculation.html')
+
+def result(request):
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+
+    context = {
+        'num1': num1,
+        'num2': num2,
+    }
+    return render(request, 'calculators/result.html', context)
