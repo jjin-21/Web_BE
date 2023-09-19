ff037d2fc7b920606210013627bb067404eed9a1
ljj1900 <ljj1900@naver.com>
2023-09-19 17:51:53 +0900

    230919

---
+++ 4.ORM/4-2/libraries/models.py
@@ -0,0 +1,13 @@
+from datetime import date
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.TextField()
+    pubdate = models.DateField()
+    price = models.IntegerField()
+    adult = models.BooleanField()
+
+    def __str__(self):
+        return self.title
